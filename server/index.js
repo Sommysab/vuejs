6a4c077e77e3f87b4097b43dc3c92b79987eb14e
sommysab <sommysabudeh@yahoo.com>
2020-01-05 22:47:41 +0100

    Add static folder and channel bad url to indexl

--- server/index.js
+++ server/index.js
@@ -5,15 +5,22 @@ const cors = require('cors');
 const app = express();
 
 
-//Middleware
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
-// post router
+// Post Router
 const posts = require('./routes/api/posts');
-
 app.use('/api/posts', posts);
 
+// Handle SPA in production
+if(process.env.NODE_ENV ==='production'){
+    // static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle Single Page Application (SPA) any route
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 const port = process.env.PORT || 5000;
 
